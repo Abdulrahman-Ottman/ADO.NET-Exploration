6e68a884493b199a4e77e589b2b6850e371e9d9e
Abdulrahman Othman <abdulrahman.othamn.sy@gmail.com>
2024-10-13 16:53:34 +0300

    udate Program.cs class

--- Program.cs
+++ Program.cs
@@ -12,50 +12,37 @@ namespace ADO.NET_Exploration
         static string connectionString = "Server=.;Database=Contacts;User Id=sa;Password=abdulrahman.10.10.2022.abdulrahman";
         static SqlConnection connection = new SqlConnection(connectionString);
 
-        static void printContactInfo(SqlDataReader reader)
+        static void getFirstNameOfContact(int id)
         {
-            int contactID = (int)reader["ContactID"];
-            string firstName = (string)reader["FirstName"];
-            string lastName = (string)reader["LastName"];
-            string email = (string)reader["Email"];
-            string phone = (string)reader["Phone"];
-            string address = (string)reader["Address"];
-            int countryID = (int)reader["CountryID"];
+            string FirstName = "";
+            string query = "select FirstName , LastName from Contacts Where ContactID = @ContactID";
 
-            Console.WriteLine($"Contact ID: {contactID}");
-            Console.WriteLine($"Name: {firstName} {lastName}");
-            Console.WriteLine($"Email: {email}");
-            Console.WriteLine($"Phone: {phone}");
-            Console.WriteLine($"Address: {address}");
-            Console.WriteLine($"Country ID: {countryID}");
-            Console.WriteLine();
-        }
+            SqlCommand command = new SqlCommand(query , connection);
 
-        static void executeAndPrint(SqlCommand command)
-        {
+            command.Parameters.AddWithValue("@ContactID" , id);
+
             try
             {
                 connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
+                object result = command.ExecuteScalar();
 
-                while (reader.Read())
+                if (result != null)
                 {
-                    printContactInfo(reader);
+                    FirstName = result.ToString();
                 }
 
-                reader.Close();
                 connection.Close();
+
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
             }
+            
+            Console.WriteLine(FirstName);
         }
-
-
         static void Main(string[] args)
         {
-
+            getFirstNameOfContact(1);
         }
     }
 }
