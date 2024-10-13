250e0fbd5987503d8c5af7fd4c79f6a1de51d6f5
Abdulrahman Othman <abdulrahman.othamn.sy@gmail.com>
2024-10-13 16:29:29 +0300

    udate Program.cs class

--- Program.cs
+++ Program.cs
@@ -10,34 +10,35 @@ namespace ADO.NET_Exploration
     internal class Program
     {
         static string connectionString = "Server=.;Database=Contacts;User Id=sa;Password=abdulrahman.10.10.2022.abdulrahman";
-        
-        static void printAllContatcts()
-        {
-            SqlConnection connection = new SqlConnection(connectionString);
-            string query = "SELECT * FROM Contacts";
-            SqlCommand command = new SqlCommand(query , connection);
+        static SqlConnection connection = new SqlConnection(connectionString);
 
+        static void printContactInfo(SqlDataReader reader)
+        {
+            int contactID = (int)reader["ContactID"];
+            string firstName = (string)reader["FirstName"];
+            string lastName = (string)reader["LastName"];
+            string email = (string)reader["Email"];
+            string phone = (string)reader["Phone"];
+            string address = (string)reader["Address"];
+            int countryID = (int)reader["CountryID"];
+
+            Console.WriteLine($"Contact ID: {contactID}");
+            Console.WriteLine($"Name: {firstName} {lastName}");
+            Console.WriteLine($"Email: {email}");
+            Console.WriteLine($"Phone: {phone}");
+            Console.WriteLine($"Address: {address}");
+            Console.WriteLine($"Country ID: {countryID}");
+            Console.WriteLine();
+        }
+        static void executeAndPrint(SqlCommand command)
+        {
             try
             {
                 connection.Open();
                 SqlDataReader reader = command.ExecuteReader();
-                while (reader.Read()) {
-                    int contactID = (int)reader["ContactID"];
-                    string firstName = (string)reader["FirstName"];
-                    string lastName = (string)reader["LastName"];
-                    string email = (string)reader["Email"];
-                    string phone = (string)reader["Phone"];
-                    string address = (string)reader["Address"];
-                    int countryID = (int)reader["CountryID"];
 
-                    
-                    Console.WriteLine($"Contact ID: {contactID}");
-                    Console.WriteLine($"Name: {firstName} {lastName}");
-                    Console.WriteLine($"Email: {email}");
-                    Console.WriteLine($"Phone: {phone}");
-                    Console.WriteLine($"Address: {address}");
-                    Console.WriteLine($"Country ID: {countryID}");
-                    Console.WriteLine();
+                while (reader.Read()) {
+                    printContactInfo(reader);
                 }
 
                 reader.Close();
@@ -47,9 +48,36 @@ namespace ADO.NET_Exploration
                 Console.WriteLine($"Error: {ex.Message}");
             }
         }
+
+        static void searchContactStartWith(string startWith)
+        {
+            string query = "select * from Contacts where FirstName Like @start + '%'";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@start", startWith);
+            executeAndPrint(command);
+        }
+
+        static void searchContactEndWith(string endWith)
+        {
+            string query = "select * from Contacts where FirstName Like  '%' + @end ";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@end", endWith);
+            executeAndPrint(command);
+        }
+
+        static void searchContactContains(string contain)
+        {
+            string query = "select * from Contacts where FirstName Like  '%' + @contain + '%' ";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@contain", contain);
+            executeAndPrint(command);
+        }
+
         static void Main(string[] args)
         {
-            printAllContatcts();
+            //searchContactStartWith("jo");
+            //searchContactEndWith("l");
+            searchContactContains("e");
         }
     }
 }
