55f3921b8bdbb4a7e22626bae997c634eb59c6c3
Abdulrahman Othman <abdulrahman.othamn.sy@gmail.com>
2024-10-14 19:39:08 +0300

    udate Program.cs class

--- Program.cs
+++ Program.cs
@@ -1,21 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Net;
 using System.Data.SqlClient;
 
-namespace ADO.NET_Exploration
+public class Program
 {
-    internal class Program
+    static string connectionString = "Server=.;Database=Contacts;User Id=sa;Password=abood";
+    static SqlConnection connection = new SqlConnection(connectionString);
+
+
+    public struct stContact
     {
-        static string connectionString = "Server=.;Database=Contacts;User Id=sa;Password=abdulrahman.10.10.2022.abdulrahman";
-        static SqlConnection connection = new SqlConnection(connectionString);
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public int CountryID { get; set; }
+    }
 
-        static void Main(string[] args)
+    static bool AddNewContact(stContact newContact)
+    {
+
+        bool added = false;
+
+        string query = @"INSERT INTO Contacts (FirstName, LastName, Email, Phone, Address, CountryID)
+                             VALUES (@FirstName, @LastName, @Email, @Phone, @Address, @CountryID)";
+
+        SqlCommand command = new SqlCommand(query, connection);
+
+        command.Parameters.AddWithValue("@FirstName", newContact.FirstName);
+        command.Parameters.AddWithValue("@LastName", newContact.LastName);
+        command.Parameters.AddWithValue("@Email", newContact.Email);
+        command.Parameters.AddWithValue("@Phone", newContact.Phone);
+        command.Parameters.AddWithValue("@Address", newContact.Address);
+        command.Parameters.AddWithValue("@CountryID", newContact.CountryID);
+
+        try
         {
+            connection.Open();
 
-            //ExecuteScalar function return the first column from the first row of the results
+            int rowsAffected = command.ExecuteNonQuery();
+
+            if (rowsAffected > 0)
+            {
+                added = true;
+            }
+
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        return added;
+    }
+
+    public static void Main()
+    {
+
+        // Create a new contact with the required information
+        stContact Contact = new stContact
+        {
+            FirstName = "Abdulrahman2",
+            LastName = "Othman2",
+            Email = "abood@example.com",
+            Phone = "1234567890",
+            Address = "123 Main Street",
+            CountryID = 1
+        };
+
+        AddNewContact(Contact);
     }
 }
