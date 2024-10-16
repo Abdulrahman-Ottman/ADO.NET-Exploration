cf2792fc35d1c62d18d223b9f9d6c96a588a025e
Abdulrahman Othman <abdulrahman.othamn.sy@gmail.com>
2024-10-16 15:24:27 +0300

    update Program.cs class

--- Program.cs
+++ Program.cs
@@ -20,22 +20,36 @@ public class Example
         EmployeesDataTable.Rows.Add(4, "Fadi JAmeel", "Egypt", 800, DateTime.Now);
         EmployeesDataTable.Rows.Add(5, "Omar Mahmoud", "Lebanon", 7000, DateTime.Now);
 
+        int EmployeesCount = 0;
+        double TotalSalaries = 0;
+        double AverageSalaries = 0;
+        double MinSalary = 0;
+        double MaxSalary = 0;
+
+
+        EmployeesCount = EmployeesDataTable.Rows.Count;
+        TotalSalaries = Convert.ToDouble(EmployeesDataTable.Compute("SUM(Salary)", string.Empty));
+        AverageSalaries = Convert.ToDouble(EmployeesDataTable.Compute("AVG(Salary)", string.Empty));
+        MinSalary = Convert.ToDouble(EmployeesDataTable.Compute("Min(Salary)", string.Empty));
+        MaxSalary = Convert.ToDouble(EmployeesDataTable.Compute("Max(Salary)", string.Empty));
 
         Console.WriteLine("\nEmployees List:\n");
 
         foreach (DataRow RecordRow in EmployeesDataTable.Rows)
         {
             //Using Index
-            // Console.WriteLine("ID: {0}\t Name : {1} \t Country: {2} \t Salary: {3} Date: {4} \t ", RecordRow[0], RecordRow[1], RecordRow[2], RecordRow[3], RecordRow[4]);
+            // Console.WriteLine("ID: {0}\t Name : {1} \t Name: {2} \t Salary: {3} Date: {4} \t ", RecordRow[0], RecordRow[1], RecordRow[2], RecordRow[3], RecordRow[4]);
 
             //Using Field Name
-            Console.WriteLine("ID: {0}\t Name : {1} \t Country: {2} \t Salary: {3} Date: {4} \t ",
-                RecordRow["ID"], RecordRow["Name"], RecordRow["Country"], RecordRow["Salary"],
-                RecordRow["Date"]);
+            Console.WriteLine("ID: {0}\t Name : {1} \t Country: {2} \t Salary: {3} Date: {4} \t ", RecordRow["ID"], RecordRow["Name"], RecordRow["Country"], RecordRow["Salary"], RecordRow["Date"]);
 
         }
 
-
+        Console.WriteLine("\nCount of Employees = " + EmployeesCount);
+        Console.WriteLine("Total Employee Salaries = " + TotalSalaries);
+        Console.WriteLine("Average Employee Salaries = " + AverageSalaries);
+        Console.WriteLine("Minimum Salary = " + MinSalary);
+        Console.WriteLine("Maximum Salary = " + MaxSalary);
 
         Console.ReadKey();
 
